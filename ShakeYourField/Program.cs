d430cddcabf24b19852ca5e4a632020c73249e4d
Ali Aras <aliarascc@gmail.com>
2016-01-11 14:27:56 +0200

    it' s a random spot generator. Developed for fun and Heat office area sharing

---
+++ ShakeYourField/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace ShakeYourField
+{
+
+    class Program
+    {
+        private static List<String> _userList;
+        public static List<String> UserList
+        {
+            get
+            {
+                return _userList;
+            }
+
+            set
+            {
+                _userList = value;
+            }
+        }
+
+        private static int _personelCount;
+        public static int PersonelCount
+        {
+            get
+            {
+                return _personelCount;
+            }
+
+            set
+            {
+                _personelCount = value;
+            }
+        }
+
+        private static bool control = true;
+        private static Random rnd;
+         private static int i = 1, y;
+
+        static void Main(string[] args)
+        {
+            UserList = new List<String>();
+            
+
+            while (control)
+            {
+                Write(i + " HEAT Personal Name : ");
+                UserList.Add(ReadLine());
+                i++;
+
+                if (UserList.Last().Equals("ok"))
+                {
+                    control = false;
+                }
+            }
+            var numbers = new List<int>(Enumerable.Range(1, UserList.Count-1));
+            numbers.Shuffle();
+
+            for (int j = 0; j < UserList.Count-1; j++)
+            {
+                WriteLine("/n"+UserList[j] + "==" + numbers.First());
+                numbers.Remove(numbers.First());
+            }
+    
+
+            ReadLine();
+
+        }
+    }
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+
+    static class MyExtensions
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
