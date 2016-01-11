ff49ed1abe3d58aab6b647a691cf56f793e353c9
Ali Aras <aliarascc@gmail.com>
2016-01-11 14:50:59 +0200

    remove unused usings

--- ShakeYourField/Program.cs
+++ ShakeYourField/Program.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static System.Console;
-using System.Security.Cryptography;
 using System.Threading;
 
 namespace ShakeYourField
